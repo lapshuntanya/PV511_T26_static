1ee2635d65b400047d9479e80aeb211b6f4568cc
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-04-21 19:51:18 +0300

    CarNumber

--- main.cpp
+++ main.cpp
@@ -9,9 +9,26 @@ int main() {
 
     vector<CarNumber> dia(10);
     for (int i =0; i < dia.size(); i++) {
-        cout << dia[i].getNumber() << endl;
-    }
+        cout << dia[i].getNumber() << " ";
+    }cout << endl;
 
+    string user;
+    cout << "Input number: ";
+    cin >> user; //"0005AA"
+
+    //[](){}
+    //vector<CarNumber>::iterator it;
+    auto it = find_if(dia.begin(), dia.end(), [user](CarNumber a) {
+        return a.getNumber() == user;
+    });
+    if (it != dia.end()) {
+        dia.erase(it);
+        dia.push_back(CarNumber());
+    }
+    cout << "__________________________________\n\n";
+    for (auto a: dia) {
+        cout << a.getNumber() << " ";
+    }cout << endl;
 
     return 0;
 }
