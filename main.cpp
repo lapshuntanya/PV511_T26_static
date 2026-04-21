fb068004222a2d14ddfc9a4eb4d89395eb305cb6
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-04-21 19:32:44 +0300

    CarNumber

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include "CarNumber.h"
+#include <vector>
+
+int CarNumber::count = 1;
+char CarNumber::seria1 = 'A';
+char CarNumber::seria2 = 'A';
 
 int main() {
+
+    vector<CarNumber> dia(10);
+    for (int i =0; i < dia.size(); i++) {
+        cout << dia[i].getNumber() << endl;
+    }
 
 
     return 0;
